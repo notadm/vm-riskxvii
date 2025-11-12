eac42526626328a25f50e794ccf2d4092edbf0e0
128x128 <adamzzzhao@gmail.com>
2025-11-12 22:17:16 +1100

    tab fix?

--- kernel.h
+++ kernel.h
@@ -38,8 +38,8 @@ i32 inKernel(u32 op, u32 v)
     clr(v); 
     switch (op)                                                                 
     {                                                                          
-	case VR_RD_CHAR: 	scanf("%c", _I08(&VM.M[v])); 		  break;
-	case VR_RD_INT: 	scanf("%d", _I32(&VM.M[v]));		  break;
+	case VR_RD_CHAR: 	scanf("%c", _I08(&VM.M[v]));              break;
+	case VR_RD_INT: 	scanf("%d", _I32(&VM.M[v]));              break;
     }
     
     return 0;
@@ -49,8 +49,8 @@ i32 heapKernel(u32 op, u32 v)
 { 
     switch (op)                                                                 
     {                                                                          
-	case VR_MALLOC: 	$rmalloc = vmalloc(*_U32(&VM.M[v])); 	  break;
-	case VR_FREE: 		vmfree(*_U32(&VM.M[v]));	  	  break;
+	case VR_MALLOC: 	$rmalloc = vmalloc(*_U32(&VM.M[v]));      break;
+	case VR_FREE: 		vmfree(*_U32(&VM.M[v]));                  break;
     }
     clr(v); 
     return 0;
@@ -79,18 +79,18 @@ i32 ALUKernel(u32 op, u32 x)
 {                                                                              
     switch (op)                                                                
     {                                                                          
-	case M_ADD:   	$rd = I32($rs1) +  I32($rs2); 		  	  break;
-	case M_SUB:   	$rd = I32($rs1) -  I32($rs2); 		  	  break;
-	case M_XOR:   	$rd = I32($rs1) ^  I32($rs2); 		  	  break;
-	case M_OR:   	$rd = I32($rs1) |  I32($rs2); 		  	  break;
-	case M_AND:   	$rd = I32($rs1) &  I32($rs2); 		  	  break;
+	case M_ADD:   	$rd = I32($rs1) +  I32($rs2);                     break;
+	case M_SUB:   	$rd = I32($rs1) -  I32($rs2);                     break;
+	case M_XOR:   	$rd = I32($rs1) ^  I32($rs2);                     break;
+	case M_OR:   	$rd = I32($rs1) |  I32($rs2);                     break;
+	case M_AND:   	$rd = I32($rs1) &  I32($rs2);                     break;
 	case M_ADDI:  	$rd = I32($rs1) +  I32(decode_imm(x));            break;
 	case M_XORI:  	$rd = I32($rs1) ^  I32(decode_imm(x));            break;
 	case M_ORI:  	$rd = I32($rs1) |  I32(decode_imm(x));            break;
 	case M_ANDI:  	$rd = I32($rs1) &  I32(decode_imm(x));            break;
-	case M_SLL:   	$rd = U32($rs1) << U32($rs2); 		  	  break;
-	case M_SRL:   	$rd = U32($rs1) >> U32($rs2);		  	  break;
-	case M_SRA:   	$rd = rotr(I32($rs1),I32($rs2));	  	  break;
+	case M_SLL:   	$rd = U32($rs1) << U32($rs2);                     break;
+	case M_SRL:   	$rd = U32($rs1) >> U32($rs2);                     break;
+	case M_SRA:   	$rd = rotr(I32($rs1),I32($rs2));                  break;
 	case M_LUI:   	$rd = decode_imm(x);                              break;
     } 									       
     NEXT_INSTRUCTION;
